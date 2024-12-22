c44dfe76cd8690342ca5512fa7a8c4c0b29a0fc9
Sneha <78603231+snehasudhakaran20@users.noreply.github.com>
2024-12-22 18:26:36 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 document.getElementById('mathForm').addEventListener('submit', async (event) => {
   event.preventDefault();
   const inputData = document.getElementById('inputData').value;
+  const inputDataGrid = document.getElementById('inputData').value;
 
   try {
     const response = await fetch('https://demosaicing-math.onrender.com/api/calculate', {
@@ -9,6 +10,13 @@ document.getElementById('mathForm').addEventListener('submit', async (event) => 
       body: JSON.stringify({ input: inputData })
     });
 
+    try {
+    const response1 = await fetch('https://demosaicing-math.onrender.com/api/fillmissingcolours', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ input: inputData })
+    });
+      
     const result = await response.json();
     document.getElementById('result').innerText = `Result: ${result.output}`;
   } catch (error) {
