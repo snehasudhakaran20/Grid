b9b297e95b863e77abe8dd412239e46922dea8c7
Sneha <78603231+snehasudhakaran20@users.noreply.github.com>
2024-12-27 03:40:22 +0530

    Create NCD.py

---
+++ backend/NCD.py
@@ -0,0 +1,8 @@
+"""
+Algo:
+1.RGB converted to CIE-LAB or CIE_LUV 
+2. calc eucledian dist btw Img1,2 in chosen color space 
+3. Normalize by dividing by max color diff or by using mean or std deviation
+4. aggregate ie sum of for entire grid
+
+"""
